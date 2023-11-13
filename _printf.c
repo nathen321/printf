19cce11c0efebe0c7c153cd32bd531d1fcc39cae
nathen321 <walidjalaly43@gmail.com>
2023-11-13 06:08:16 -0800

    testing string and char output

--- _printf.c
+++ _printf.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdarg.h>
+#include <string.h>
 #include "main.h"
 
 /**
@@ -11,8 +12,65 @@ void _printf(const char *format, ...)
 {
 	fm_t forma_type[] = {
 		{ "c", print_char },
-		{ "i", print_integer },
-		{ "f", print_float },
 		{ "s", print_string }
 	};
+	unsigned int sum = 0, i = 0, j = 0;
+	va_list ap;
+
+	va_start(ap, format);
+
+	while (format != NULL && format[i])
+	{
+		j = 0;
+		while (j < 2)
+		{
+			if (format[i] == '%')
+			{
+				if (format[i + 1] == *forma_type[j].id)
+				{
+					sum += forma_type[j].f(ap);
+					i++;
+				}
+			}
+			else
+			{
+				_putchar('l');
+			}
+			j++;
+		}
+		i++;
+	}
+	va_end(ap);
+	_putchar ('\n');
+}
+
+/**
+ * print_char - print a formated output?
+ * @ap : ....
+ * Return: ......
+ */
+int print_char(va_list ap)
+{
+	_putchar(va_arg(ap, int));
+	return (1);
+}
+
+/**
+ * print_string - print a formated output?
+ * @ap : ....
+ * Return: ....
+ */
+int print_string(va_list ap)
+{
+	unsigned int sum = 0, i = 0;
+	char *str;
+
+	str = va_arg(ap, char *);
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+		sum++;
+	}
+	return (sum);
 }
--- main.h
+++ main.h
@@ -1,9 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 typedef struct foramat_type
 {
 	char *id;
-	void (*f)(va_list args, char *sep);
+	int (*f)(va_list args);
 } fm_t;
+
+void _printf(const char *format, ...);
+int _putchar(char c);
+int print_string(va_list ap);
+int print_char(va_list ap);
 #endif
